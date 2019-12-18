bfeb691fb2cbe3b103332e8a10e248288bcd956a
Martin Seon <hyunmui@outlook.kr>
2019-12-18 16:59:25 +0900

    first spring program

---
+++ SpringHello/src/main/java/springHello/Hello.java
@@ -0,0 +1,9 @@
+package springHello;
+
+public class Hello {
+	
+	public String sayHello(String name) {
+		return "Hello? " + name + ", Sir!";
+	}
+
+}
---
+++ SpringHello/src/main/java/springHello/Main.java
@@ -0,0 +1,15 @@
+package springHello;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+		Hello hello = (Hello) ctx.getBean("hello");
+		String message = hello.sayHello("hyunmui");
+		System.out.println(message);
+	}
+
+}
