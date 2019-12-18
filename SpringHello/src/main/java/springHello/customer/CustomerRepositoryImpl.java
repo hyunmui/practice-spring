70ef8c777ed7dbd6c9d7144b40a43a23e2a0e043
Martin Seon <hyunmui@outlook.kr>
2019-12-18 18:16:30 +0900

    3.4 Di for field / property value

--- SpringHello/src/main/java/springHello/customer/CustomerRepositoryImpl.java
+++ SpringHello/src/main/java/springHello/customer/CustomerRepositoryImpl.java
@@ -7,6 +7,27 @@ import springHello.common.Pageable;
 
 public class CustomerRepositoryImpl implements CustomerRepository {
 
+	private String driverClassName;
+	private String url;
+	private String username;
+	private String password;
+
+	public void setDriverClassName(String driverClassName) {
+		this.driverClassName = driverClassName;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	@Override
 	public CustomerEntity findOne(long id) {
 		CustomerEntity customer = new CustomerEntity();
